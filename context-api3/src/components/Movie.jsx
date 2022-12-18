b9b67beafea15556cf2565f4a9f941647cf0c9ad
Fernando Warno <ferwar@hotmail.com>
2022-12-18 06:53:55 -0300

    UserContext para boton favoritos

--- context-api3/src/components/Movie.jsx
+++ context-api3/src/components/Movie.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../contexts/UserContext';
 
 const Movie = ({ movie }) => {
-  console.log('Movie');
+  // console.log('Movie');
+
+  // Aqui vamos a extraer la funcion toggleFavoriteMovieToUser
+  // que esta en el contextoONconst context = useContext(contextValue); UserContext
+  // const data = Como ña data es un objeto lo ponemos de una vez entre llaves
+  const { user, toggleFavoriteMovieToUser } = useContext(UserContext);
+
   const imgStyles = {
     height: '260px',
     objectFit: 'cover',
   }
 
-  const isFavorite = false;
+  // const isFavorite = false;
+  // favoriteMovies es un array de ids de peliculas favoritas
+  // Para comprobar si este arreglo tiene el id de la pelicula que estamos renderizando
+  // usamos el metodo includes. Si es true el boton se pone verde
+  // Hay que traer el objeto user del contexto
+  // ? Pregunta si es un arreglo si es un arreglo hace la operacion
+  // Tambien es recomendaable usar el signo ? para preguntar si el objeto user tiene la propiedad favorite
+  const isFavorite = user?.favorite?.includes(movie.id);
 
   return (
     <div className='card' >
@@ -17,11 +31,21 @@ const Movie = ({ movie }) => {
         className='card-img-top'
         style={imgStyles}
       />
-      <div className='card-body' >
+      <div className='card-body'>
         <h4>{movie.title}</h4>
-        <button className={`btn ${isFavorite ? 'btn-success' : 'btn-outline-primary'}`} >
-          Favorito
-        </button>
+        { user?.id &&
+        
+          <button className={
+            `btn ${isFavorite ? 'btn-success' : 'btn-outline-primary'}`}
+          // Tengo que extraer la funcion para poder usarla
+          // Aqui necesitamos enviarle el movie.id
+          // Como estamos usando los () en la funcion, tenemos que usar una funcion   anonima
+          // para que no se ejecute la funcion al momento de renderizar el componente
+            onClick={() => toggleFavoriteMovieToUser(movie.id)}
+          >
+            Favorito
+          </button>
+        }
       </div>
     </div>
   )
--- context-api3/src/contexts/UserContext.jsx
+++ context-api3/src/contexts/UserContext.jsx
@@ -4,12 +4,12 @@ import React, { createContext, useState } from 'react';
 const UserContext = createContext();
 
 // El objeto initialUser tiene la informacion del usuario
-const initialUser = { id: 1, name: 'Fernando', favorites: [1,2] };
+const initialUser = { id: 1, name: 'Fernando', favoriteMovies: [1,2] };
 
 // La propiedad children contiene la informacion del Navbar y del MovieList
 const UserProvider = ({ children }) => {
 
-  const [user, setUser] = useState(initialUser);
+  const [user, setUser] = useState(initialUser)
 
   // Creamos la funcion login
   const login = () => {
@@ -21,8 +21,34 @@ const UserProvider = ({ children }) => {
     setUser(null);
   }
 
+  // Creamos una funcion para Favoritos
+  // Aqui recibimos el id de la pelicula que queremos agregar a favoritos
+  // Esta funcion la vamos a necesitar en una movie especifica entonces
+  // nos vamos al componente Movie.
+  const toggleFavoriteMovieToUser = (movieId) => {
+    // alert(movieId);
+
+    // Tenemos que controlar
+    // Vamos a consultar si el usuario tiene la pelicula con el id que estamos recibiendo
+    // Con el signo 2do? preguntamos si el usuario tiene la propiedad favoriteMovies
+    // si la tiene hace la operacion.
+    const isFavorite = user.favoriteMovies.includes(movieId);
+    const favoriteMovies = isFavorite
+      // si es true, tenemos que quitarla del arreglo de favoritos
+      ? user.favoriteMovies.filter(favMovId => favMovId !== movieId) // Delete movieId from favoriteMovies
+      // si es false, tenemos que agregarla al arreglo de favoritos
+      : [...user.favoriteMovies, movieId]; // Add movieId to favoriteMovies
+
+    setUser({
+      // Tenemos que setear al usuario con el mismo arreglo pero con el nuevo
+      // arreglo de peliculas favoritas
+      ...user,
+      favoriteMovies: favoriteMovies,
+    })
+  }
+
   // Pasamos las 2 funciones que vamos a consumir en el componente Navbar
-  const data = { user, login, logout }
+  const data = { user, login, logout, toggleFavoriteMovieToUser }
 
   return (
     <UserContext.Provider value={ data }>
